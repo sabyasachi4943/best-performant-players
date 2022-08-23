88fef673d4b95ae43422c537eab1d5d09aa11a7a
Sabyasachi Sarkar <54597679+sabyasachi4943@users.noreply.github.com>
2022-08-23 21:19:33 +0600

    budget script added

--- js/budget.js
+++ js/budget.js
@@ -1,1 +1,28 @@
 console.log("budget connected");
+const perPlayerCost = document.getElementById("perPlayerCost");
+const playerExpenses = document.getElementById("playerExpenses");
+document
+  .getElementById("calculatePlayerCost")
+  .addEventListener("click", function () {
+    const playerExpense = parseInt(perPlayerCost.value) * 5;
+    playerExpenses.innerText = playerExpense;
+  });
+
+function totalcalculation(a, b, c) {
+  return a + b + c;
+}
+
+document
+  .getElementById("calculateTotal")
+  .addEventListener("click", function () {
+    const manager = document.getElementById("manager");
+    const coach = document.getElementById("coach");
+    const playerExpense = parseInt(perPlayerCost.value) * 5;
+    const managerCost = parseInt(manager.value);
+    const coachCost = parseInt(coach.value);
+    document.getElementById("totalExpenses").innerText = totalcalculation(
+      managerCost,
+      coachCost,
+      playerExpense
+    );
+  });
